48f7ed47be36ba85b1de52257f651c5552085f56
Dale Allan <dale@cybercat.co.za>
2018-02-19 09:51:20 +0200

    laratrust routes

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,12 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(['middleware' => 'auth:web'], function() {
+    Route::get('/home', 'HomeController@index')->name('home');
+
+    Route::resource('users', 'UserController');
+    Route::resource('roles', 'RoleController');
+    Route::resource('permissions', 'PermissionController');
+    Route::resource('clients', 'ClientController');
+});
